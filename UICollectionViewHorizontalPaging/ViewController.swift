a7daafa264c2d22205dc1740c4ae3b45f13a2c13
Jeremy <woonjeremy10@gmail.com>
2021-01-18 17:01:52 +0800

    Update ViewController.swift

--- UICollectionViewHorizontalPaging/ViewController.swift
+++ UICollectionViewHorizontalPaging/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    //MARK: Connect CollectionView Outlet
+    @IBOutlet weak var SampleCollectionView: UICollectionView!
+    var CollectionData = ["Item 1","Item 2","Item 3","Item 4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: CollectionView dataSource & Delegate
+        self.SampleCollectionView.dataSource = self
+        self.SampleCollectionView.delegate = self
+        
+        //MARK: Register Cell Nib
+        self.SampleCollectionView.register(UINib(nibName: "SampleCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
     }
-
+    
+    //MARK: Configure CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return CollectionData.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = SampleCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SampleCollectionViewCell
+        cell.title.text = CollectionData[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: SampleCollectionView.frame.width - 30, height: SampleCollectionView.frame.height - 15)
+    }
 
 }
 

465e497fdf6bd281efe702ac747e14108f470199
FatCucumber <woonjeremy10@gmail.com>
2021-01-18 16:17:58 +0800

    Initial Commit

---
+++ UICollectionViewHorizontalPaging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewHorizontalPaging
+//
+//  Created by Jeremy on 18/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

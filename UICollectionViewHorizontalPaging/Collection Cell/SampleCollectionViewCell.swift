cf63c4d9453da87f9a763f628b2d19def566f60d
FatCucumber <woonjeremy10@gmail.com>
2021-01-18 17:03:17 +0800

    Missing file

---
+++ UICollectionViewHorizontalPaging/Collection Cell/SampleCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SampleCollectionViewCell.swift
+//  UICollectionViewHorizontalPaging
+//
+//  Created by Jeremy on 18/01/2021.
+//
+
+import UIKit
+
+class SampleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UICollectionViewHorizontalPaging/Round Corner Extension.swift
@@ -0,0 +1,90 @@
+//
+//  Round Corner Extension.swift
+//  UICollectionViewHorizontalPaging
+//
+//  Created by Jeremy on 18/01/2021.
+//
+
+import UIKit
+import Foundation
+
+//MARK: - Extension for Corner Round
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.masksToBounds = false
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.masksToBounds = false
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.masksToBounds = false
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
